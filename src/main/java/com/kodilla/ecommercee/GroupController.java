2efa34760af371991c68ffc213dd098f8d299ba0
Wojciech Mysliwiec <wojmys@gmail.com>
2023-08-21 08:59:07 +0200

    first commit

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -1,4 +1,13 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.web.bind.annotation.RestController;
+
+//lista wszystkich grup
+//        dodanie grupy
+//        wyświetlenie grupy o podanym id
+//        aktualizacja wybranej grupy
 public class GroupController {
+
+
+
 }
