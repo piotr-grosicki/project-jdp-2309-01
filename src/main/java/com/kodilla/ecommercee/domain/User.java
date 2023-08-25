9c63490a1829cb994fe8bf5c6656e6c7599da0f4
Patryk Potrykus <potrykus.patrykk@gmail.com>
2023-08-25 15:44:30 +0200

    User entity - implementation

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -2,13 +2,63 @@ package com.kodilla.ecommercee.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
+@Setter
+@Entity
+@Table(name = "USERS")
 public class User {
-    private int id;
-    String email;
-    String username;
-    String password;
-    boolean isBlocked;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
+    @Column(name = "ID", unique = true)
+    private Long id;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Column(name = "GENERATED_KEY")
+    private String generatedKey;
+
+    @Column(name = "EXPIRATION_DATE")
+    private Date expirationDate;
+
+    @NotNull
+    @Column(name = "USER_STATUS")
+    private boolean isBlocked;
+
+    @OneToMany(
+            targetEntity = Cart.class,
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    private List<Cart> carts = new ArrayList<>();
+
+    @OneToMany(
+            targetEntity = Order.class,
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    private List<Order> orders = new ArrayList<>();
+
 }
