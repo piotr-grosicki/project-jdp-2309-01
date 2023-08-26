9fc9ccf36666d4f91953d8f65295cbe59f191edc
enmebaraggesi <koniec.poczatek@gmail.com>
2023-08-26 15:45:52 +0200

    JDP230901-14 encja + repo + odkomentowanie User + usunięto duplikat klasy OrderController

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -2,16 +2,41 @@ package com.kodilla.ecommercee.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
-import java.util.List;
 
 @Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "CARTS")
 public class Cart {
     
-    private Long id;
-    private int userId;
-    private LocalDate created;
-    private List<Product> products;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
+    Long id;
+    
+    @ManyToOne(targetEntity = User.class,
+               cascade = CascadeType.ALL,
+               fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    User user;
+    
+    @NotNull
+    LocalDate created;
+    
+    /*@ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "JOIN_CART_PRODUCT",
+            joinColumns = @JoinColumn(name = "CART_ID"),
+            inverseJoinColumns = @JoinColumn(name = "PRODUCT_ID")
+    )
+    List<Product> products = new ArrayList<>();*/
+    
 }
