30a7262f793bfbaede50b6090645ed6db242ad8e
Maciej <129668044+CzaHa02@users.noreply.github.com>
2023-08-29 18:50:20 +0200

    POPRAWIONO TASK JDP230901-11 Order Entity (#17)
    * TASK JDP230901-11 Order Entity
    
    * TASK JDP230901-11 Order Entity
    
    * POPRAWIONO TASK JDP230901-11 Order Entity
    
    * POPRAWIONO TASK JDP230901-11 Order Entity
    
    * POPRAWIONO TASK JDP230901-11 Order Entity products

--- src/main/java/com/kodilla/ecommercee/domain/Order.java
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -1,18 +1,41 @@
 package com.kodilla.ecommercee.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.*;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+@Entity
+
 @Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "ORDERS")
 public class Order {
-    private Long id;
-    private User user;
-     private Date orderDate;
-     private String status;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @NotNull
+  private Long id;
 
-     private List<Product>orderProducts;
+  @ManyToOne
+  private User user;
 
+  private Date orderDate;
+
+  private String status;
+
+
+
+  @ManyToMany(
+          targetEntity =Product.class,
+          mappedBy = "orders",
+          cascade = CascadeType.ALL,
+          fetch = FetchType.LAZY
+  )
+  private List<Product> products = new ArrayList<>();
 }
+
