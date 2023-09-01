0aea0613037fdfb1ed8ae4a65ae1a3b62e51cfff
Wojciech Mysliwiec <wojmys@gmail.com>
2023-09-01 10:45:22 +0200

    add @Override equals and hashCode in Group Class

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -6,6 +6,7 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Builder
@@ -31,4 +32,23 @@ public class Group {
     )
     private List<Product> products = new ArrayList<>();
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Group group = (Group) o;
+
+        if (!Objects.equals(id, group.id)) return false;
+        if (!Objects.equals(name, group.name)) return false;
+        return Objects.equals(products, group.products);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (products != null ? products.hashCode() : 0);
+        return result;
+    }
 }
