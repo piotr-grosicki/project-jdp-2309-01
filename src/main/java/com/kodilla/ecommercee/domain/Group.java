d7b1aac35377f721082ae3030fe0d6ccb23596cf
enmebaraggesi <koniec.poczatek@gmail.com>
2023-09-03 21:24:35 +0200

    JDP230901-17 poprawione/na nowo

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -6,28 +6,50 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name="PRODUCT_GROUPS")
 public class Group {
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @NotNull
     @Column(name = "GROUP_ID", unique = true)
     private Long id;
     private String name;
-
+    
     @OneToMany(
             targetEntity = Product.class,
             mappedBy = "group",
             cascade = CascadeType.ALL,
             fetch = FetchType.LAZY
     )
+    @Builder.Default
     private List<Product> products = new ArrayList<>();
-
+    
+    @Override
+    public boolean equals(Object object) {
+        
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        
+        Group group = (Group) object;
+        
+        if (!Objects.equals(id, group.id)) return false;
+        return Objects.equals(name, group.name);
+    }
+    
+    @Override
+    public int hashCode() {
+        
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
 }
--- src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
@@ -6,13 +6,10 @@ import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 import java.util.List;
-import java.util.Optional;
 
 @Transactional
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
+    
     List<Product> findAll();
-    Optional<Product> findById(Long id);
-    Product save(Product product);
-    void deleteById(Long id);
 }
