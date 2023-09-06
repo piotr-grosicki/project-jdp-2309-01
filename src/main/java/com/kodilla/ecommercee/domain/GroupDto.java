88847912e414c5a336d95bc9391d42106e84a6e6
Wojciech Mysliwiec <wojmys@gmail.com>
2023-09-06 09:50:03 +0200

    add GroupMapper Class, add @Builder upon GroupDto and ProductDto

--- src/main/java/com/kodilla/ecommercee/domain/GroupDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/GroupDto.java
@@ -1,12 +1,14 @@
 package com.kodilla.ecommercee.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class GroupDto {
 
     private Long id;
---
+++ src/main/java/com/kodilla/ecommercee/mapper/GroupMapper.java
@@ -0,0 +1,57 @@
+package com.kodilla.ecommercee.mapper;
+
+import com.kodilla.ecommercee.domain.Group;
+import com.kodilla.ecommercee.domain.GroupDto;
+import com.kodilla.ecommercee.domain.Product;
+import com.kodilla.ecommercee.domain.ProductDto;
+
+import java.util.List;
+
+public class GroupMapper {
+
+
+    // ----------------------------------------------------------------------------------
+    //  delete if ProductMapper implemented
+
+    public Product mapToProduct(final ProductDto productDto) {
+        return Product.builder()
+                .id(productDto.getId())
+                .build();
+    }
+
+    public ProductDto mapToProductDto (final Product product) {
+        return ProductDto.builder()
+                .id(product.getId())
+                .build();
+    }
+
+    public List<ProductDto> mapToProductDtoList(final List<Product> listOfProducts) {
+        return listOfProducts.stream()
+                .map(this::mapToProductDto)
+                .toList();
+    }
+
+    // --------------------------------------------------------------------------------
+
+    public Group maptoGroup(final GroupDto groupDto){
+        return Group.builder()
+                .id(groupDto.getId())
+                .name(groupDto.getName())
+                .products(maptoGroup(groupDto).getProducts())
+                .build();
+    }
+
+    public GroupDto mapToGroupDto(final Group group){
+        return GroupDto.builder()
+                .id(group.getId())
+                .name(group.getName())
+                .listOfProducts(mapToProductDtoList(group.getProducts()))
+                .build();
+    }
+
+    public List<GroupDto> mapToGroupDtoList(final List<Group> groupList) {
+        return groupList.stream()
+                .map(this::mapToGroupDto)
+                .toList();
+    }
+}
