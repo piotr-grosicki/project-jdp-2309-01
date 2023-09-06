966d469cedcbac81d2507a2c586ec359d815f553
Wojciech Mysliwiec <wojmys@gmail.com>
2023-09-06 15:40:36 +0200

    add @Setter upon GroupDto Class

--- src/main/java/com/kodilla/ecommercee/domain/GroupDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/GroupDto.java
@@ -3,11 +3,14 @@ package com.kodilla.ecommercee.domain;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
+@Setter
 @Builder
 public class GroupDto {
 
